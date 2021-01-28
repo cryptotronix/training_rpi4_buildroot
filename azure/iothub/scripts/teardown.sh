75432413ea2b04f3cf1c20fe04678d4b4f75c412
gripefruits <strforge@protonmail.com>
2021-01-27 18:51:10 -0800

    FIX bugs

--- azure/iothub/scripts/teardown.sh
+++ azure/iothub/scripts/teardown.sh
@@ -2,6 +2,11 @@
 
 set -e
 
+if [ -z $AZ_RESOURCE_GROUP_NAME ];then
+	echo "please set AZ_RESOURCE_GROUP_NAME. exiting..."
+	exit 1
+fi
+
 echo "removing objects from azure..."
 az group delete -n $AZ_RESOURCE_GROUP_NAME -y
 echo "done!"
